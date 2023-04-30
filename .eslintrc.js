1191bf3e818a9fcffc371022c03a603621751021
Andrey <peckondratov@gmail.com>
2023-04-30 23:25:07 +0500

    feat(eslint): create eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,36 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 1,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 1,
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'arrow-parens': ['error', 'always'],
+    'prefer-destructuring': [
+      'error',
+      {
+        array: true,
+        object: true,
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    'max-len': ['error', { code: 120 }],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         mocha: true,
--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,45 @@
-import { createRouter, createWebHistory } from "vue-router";
-import CatalogView from "@/views/CatalogView.vue";
-import ProductView from "@/views/ProductView.vue";
-import СartView from "@/views/CartView.vue";
-import OrderNewView from "@/views/OrderNewView.vue";
-import OrderView from "@/views/OrderView.vue";
-import NotFound from "@/views/NotFound.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import CatalogView from '@/views/CatalogView.vue';
+import ProductView from '@/views/ProductView.vue';
+import СartView from '@/views/CartView.vue';
+import OrderNewView from '@/views/OrderNewView.vue';
+import OrderView from '@/views/OrderView.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
-    path: "/",
-    name: "catalogView",
+    path: '/',
+    name: 'catalogView',
     component: CatalogView,
-    meta: { name: "Каталог" },
+    meta: { name: 'Каталог' },
   },
   {
-    path: "/product/:id",
-    name: "productView",
+    path: '/product/:id',
+    name: 'productView',
     component: ProductView,
-    meta: { name: "Продукт" },
+    meta: { name: 'Продукт' },
   },
   {
-    path: "/cart",
-    name: "cartView",
+    path: '/cart',
+    name: 'cartView',
     component: СartView,
-    meta: { name: "Корзина" },
+    meta: { name: 'Корзина' },
   },
   {
-    path: "/order/new",
-    name: "orderNewView",
+    path: '/order/new',
+    name: 'orderNewView',
     component: OrderNewView,
-    meta: { name: "Новый заказ" },
+    meta: { name: 'Новый заказ' },
   },
   {
-    path: "/order/:id",
-    name: "orderView",
+    path: '/order/:id',
+    name: 'orderView',
     component: OrderView,
-    meta: { name: "Заказ" },
+    meta: { name: 'Заказ' },
   },
   {
-    path: "/:pathMatch(.*)*",
-    name: "notFound",
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
     component: NotFound,
   },
 ];
